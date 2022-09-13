b8d17a0e27650dcb7e35204295b4a4002a2a5798
Egor Vorontsov <egor20vv@gmail.com>
2022-09-14 03:42:56 +0700

    Zipping test is complete

--- CLI_FilesArchivator/Program.cs
+++ CLI_FilesArchivator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CLI_FilesArchivator.Settings;
 using CLI_FilesArchivator.Settings.Types;
 using Newtonsoft.Json;
+using System.IO.Compression;
 
 // // Methods // //
 
@@ -20,9 +21,24 @@ static async Task CreateDefaultSettingsDataFile(string settingsFileName)
     await settings.WriteSettingsFileAsync(GetDefaultSettingsData());
 }
 
+//enum RewriteIssueAnswer
+//{
+//    NoActions, DeleteCreateNew, ReserveCreateNew
+//};
+
+static async Task<bool> FileAlreadyExistsRewriteIssue(string fileKind, string fileName)
+{
+    Console.WriteLine("The {0} file \"{1}\" is already exists. Do you want rewrite it? [y/n]", fileKind, fileName);
+    var answer = Console.ReadLine()?.ToLower();
+    return answer?.StartsWith("y") ?? false;
+}
+
+
+
 
 // // Main // //
 
+// get settings
 var settingsData = Task.Run(async () =>
 {
     var settingsFileName = "settings.json";
@@ -38,6 +54,7 @@ var settingsData = Task.Run(async () =>
         await CreateDefaultSettingsDataFile(settingsFileName);
 
         Console.Error.WriteLine("Settings file was not found, new will be created");
+        Environment.Exit(1);
         return null;
     }
     catch (IOException e)
@@ -47,6 +64,7 @@ var settingsData = Task.Run(async () =>
         await CreateDefaultSettingsDataFile(settingsFileName);
 
         Console.Error.WriteLine("Settings file can't to be opened, new will be created");
+        Environment.Exit(1);
         return null;
     }
     catch (JsonSerializationException e)
@@ -57,11 +75,73 @@ var settingsData = Task.Run(async () =>
         await CreateDefaultSettingsDataFile(settingsFileName);
 
         Console.Error.WriteLine("Settings file deserialization was unsuccessful, new file will be created, old got a name ({0})", "old_" + settingsFileName);
+        Environment.Exit(1);
         return null;
     }
 }).GetAwaiter().GetResult();
 
+// check settings file
+Task.Run(() =>
+{
+    // check destination
+    if (!Directory.Exists(settingsData!.DestinationFolder))
+        throw new DirectoryNotFoundException(settingsData!.DestinationFolder);
+
+    // check sources
+    var existingSources = new List<string>();
+    foreach (var source in settingsData!.Sources)
+    {
+        if (Directory.Exists(source) || File.Exists(source))
+            existingSources.Add(source);
+    }
+
+    if (existingSources.Count() == 0)
+        throw new Exception("source files was not found");
+
+    settingsData!.Sources = existingSources;
+
+}).GetAwaiter().GetResult();
+
+
+// zip
+static async Task WriteSourcesToArchive(IEnumerable<string> filesList, ZipArchive archive, string? dirFrom = null)
+{
+    foreach (var source in filesList)
+    {
+        if (File.Exists(source))
+        {
+            using var sourceReader = new StreamReader(File.OpenRead(source));
+
+            var entryName = dirFrom == null ?
+                Path.GetFileName(source) :
+                Path.GetRelativePath(Directory.GetParent(dirFrom)!.FullName, source);
+
+            var entry = archive.CreateEntryFromFile(source, entryName);
+
+            Console.WriteLine("Info: File \"{0}\" archieved", source);
+        }
+        else if (Directory.Exists(source))
+        {
+            var entryName = dirFrom == null ?
+               Path.GetFileName(source) + '\\' :
+               Path.Combine(Path.GetRelativePath(Directory.GetParent(dirFrom)!.FullName, source));
+
+            await WriteSourcesToArchive(Directory.GetFiles(source), archive, dirFrom ?? source);
+            await WriteSourcesToArchive(Directory.GetDirectories(source), archive, dirFrom ?? source);
+        }
+    }
+}
+
+Task.Run(async () =>
+{
+    var zipFileFullName = Path.Combine(settingsData!.DestinationFolder, settingsData.ZipFileName + ".zip");
+
+    using var archive = new ZipArchive(File.Open(zipFileFullName, FileMode.OpenOrCreate, FileAccess.ReadWrite), ZipArchiveMode.Update);
+
+    await WriteSourcesToArchive(settingsData!.Sources, archive);
+
+}).GetAwaiter().GetResult();
 
 // // Log out // //
 
-Console.WriteLine(settingsData);
+// Console.WriteLine(settingsData);
