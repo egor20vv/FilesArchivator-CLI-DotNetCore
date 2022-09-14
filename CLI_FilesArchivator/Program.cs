6021039ecbd45f9c017c5a37699732fe0275738d
Egor Vorontsov <egor20vv@gmail.com>
2022-09-14 22:44:07 +0700

    SettingsHandler static class was created

--- CLI_FilesArchivator/Program.cs
+++ CLI_FilesArchivator/Program.cs
@@ -1,147 +1,34 @@
-﻿using CLI_FilesArchivator.Settings;
-using CLI_FilesArchivator.Settings.Types;
-using Newtonsoft.Json;
-using System.IO.Compression;
-
-// // Methods // //
-
-static SettingsData GetDefaultSettingsData() =>
-    new SettingsData
-    {
-        Sources = new[] { "path/to/source", "path/to/source" },
-        DestinationFolder = "path/to/destination/folder",
-        ZipFileName = "name_of_zip_file",
-        LogOptions = new[] { LogOption.Info, LogOption.Debug, LogOption.Error }
-    };
-
-static async Task CreateDefaultSettingsDataFile(string settingsFileName)
-{
-    await using var settings = new Settings();
-    settings._File = File.Open(settingsFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
-    await settings.WriteSettingsFileAsync(GetDefaultSettingsData());
-}
-
-//enum RewriteIssueAnswer
-//{
-//    NoActions, DeleteCreateNew, ReserveCreateNew
-//};
-
-static async Task<bool> FileAlreadyExistsRewriteIssue(string fileKind, string fileName)
-{
-    Console.WriteLine("The {0} file \"{1}\" is already exists. Do you want rewrite it? [y/n]", fileKind, fileName);
-    var answer = Console.ReadLine()?.ToLower();
-    return answer?.StartsWith("y") ?? false;
-}
-
-
-
+﻿using CLI_FilesArchivator;
+using CLI_FilesArchivator.Logging;
+using CLI_FilesArchivator.Zipper;
 
 // // Main // //
 
+// set logger
+await using var logger = new Logger().ConfigureConsoleOut();
+
 // get settings
-var settingsData = Task.Run(async () =>
-{
-    var settingsFileName = "settings.json";
+var settingsData = await SettingsHandler.GetSettingsData("settings.json", logger);
 
-    try
-    {
-        await using var settings = new Settings(settingsFileName);
-        return await settings.ReadSettingsFileAsync();
-    }
-    catch (FileNotFoundException e)
-    {
-        // create new defualt file
-        await CreateDefaultSettingsDataFile(settingsFileName);
-
-        Console.Error.WriteLine("Settings file was not found, new will be created");
-        Environment.Exit(1);
-        return null;
-    }
-    catch (IOException e)
-    {
-        // delete preview, create new default file
-        File.Delete(settingsFileName);
-        await CreateDefaultSettingsDataFile(settingsFileName);
-
-        Console.Error.WriteLine("Settings file can't to be opened, new will be created");
-        Environment.Exit(1);
-        return null;
-    }
-    catch (JsonSerializationException e)
-    {
-        // reserve preview, create new default file
-        File.Delete("old_" + settingsFileName);
-        File.Move(settingsFileName, "old_" + settingsFileName);
-        await CreateDefaultSettingsDataFile(settingsFileName);
-
-        Console.Error.WriteLine("Settings file deserialization was unsuccessful, new file will be created, old got a name ({0})", "old_" + settingsFileName);
-        Environment.Exit(1);
-        return null;
-    }
-}).GetAwaiter().GetResult();
-
+// configure logger for a log file
+await logger.ConfigureLogFile(settingsData?.LogOptions);
+ 
 // check settings file
-Task.Run(() =>
-{
-    // check destination
-    if (!Directory.Exists(settingsData!.DestinationFolder))
-        throw new DirectoryNotFoundException(settingsData!.DestinationFolder);
-
-    // check sources
-    var existingSources = new List<string>();
-    foreach (var source in settingsData!.Sources)
-    {
-        if (Directory.Exists(source) || File.Exists(source))
-            existingSources.Add(source);
-    }
-
-    if (existingSources.Count() == 0)
-        throw new Exception("source files was not found");
-
-    settingsData!.Sources = existingSources;
-
-}).GetAwaiter().GetResult();
-
+settingsData = await SettingsHandler.ActualSettingsData(settingsData, logger);
 
 // zip
-static async Task WriteSourcesToArchive(IEnumerable<string> filesList, ZipArchive archive, string? dirFrom = null)
-{
-    foreach (var source in filesList)
-    {
-        if (File.Exists(source))
-        {
-            using var sourceReader = new StreamReader(File.OpenRead(source));
-
-            var entryName = dirFrom == null ?
-                Path.GetFileName(source) :
-                Path.GetRelativePath(Directory.GetParent(dirFrom)!.FullName, source);
-
-            var entry = archive.CreateEntryFromFile(source, entryName);
-
-            Console.WriteLine("Info: File \"{0}\" archieved", source);
-        }
-        else if (Directory.Exists(source))
-        {
-            var entryName = dirFrom == null ?
-               Path.GetFileName(source) + '\\' :
-               Path.Combine(Path.GetRelativePath(Directory.GetParent(dirFrom)!.FullName, source));
-
-            await WriteSourcesToArchive(Directory.GetFiles(source), archive, dirFrom ?? source);
-            await WriteSourcesToArchive(Directory.GetDirectories(source), archive, dirFrom ?? source);
-        }
-    }
-}
-
-Task.Run(async () =>
+if (settingsData != null)
 {
     var zipFileFullName = Path.Combine(settingsData!.DestinationFolder, settingsData.ZipFileName + ".zip");
+    await using var zipFile = File.Open(zipFileFullName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    await using var zipper = new Zipper(zipFile, logger);
+    await zipper.WriteArchive(settingsData!.Sources);
 
-    using var archive = new ZipArchive(File.Open(zipFileFullName, FileMode.OpenOrCreate, FileAccess.ReadWrite), ZipArchiveMode.Update);
+    Console.WriteLine("\nPress any key to close this window . . .");
+}
+else
+{
+    Console.WriteLine("Program exits with code error 1. \n\nPress any key to close this window . . .");
+}
 
-    await WriteSourcesToArchive(settingsData!.Sources, archive);
-
-}).GetAwaiter().GetResult();
-
-// // Log out // //
-
-// Console.WriteLine(settingsData);
+Console.ReadLine();
---
+++ CLI_FilesArchivator/SettingsHandler.cs
@@ -0,0 +1,148 @@
+﻿using CLI_FilesArchivator.Logging;
+using CLI_FilesArchivator.Settings;
+using CLI_FilesArchivator.Settings.Types;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI_FilesArchivator;
+
+public static class SettingsHandler
+{
+    public static async Task<SettingsData?> GetSettingsData(string settingsFileName, Logger logger)
+    {
+        try
+        {
+            await using var settingsFile = File.Open(settingsFileName, FileMode.Open, FileAccess.ReadWrite);
+            await logger.Debug("Settings file opened");
+
+            await using var settings = new Settings.Settings(settingsFile);
+            var settingsData = await settings.ReadSettingsFileAsync();
+            await logger.Debug("Settings readed successfully");
+
+            return settingsData;
+        }
+        catch (FileNotFoundException)
+        {
+            // create new defualt file
+            await logger.Error("Settings file was not found, new will be created");
+
+            await CreateDefaultSettingsDataFile(settingsFileName);
+            await logger.Debug("New settings file was created");
+
+            //Environment.Exit(1);
+            return null;
+        }
+        catch (IOException)
+        {
+            // delete preview, create new default file
+            await logger.Error("Settings file can't to be opened");
+            //Environment.Exit(1);
+            return null;
+        }
+        catch (JsonSerializationException e)
+        {
+            // reserve preview, create new default file
+            await logger.Error(string.Format("Settings file deserialization was unsuccessful: {0}", e.Message));
+            if (YesNoIssue(string.Format("Do you want to reserve old to \"{0}\" and create a new settings file?", "old_" + settingsFileName)))
+            {
+                await File.WriteAllBytesAsync("old_" + settingsFileName, await File.ReadAllBytesAsync(settingsFileName));
+                await CreateDefaultSettingsDataFile(settingsFileName);
+                await logger.Debug("Actual settings copied to \"old_settings.txt\" file, new one has been created");
+            }
+
+            //Environment.Exit(1);
+            return null;
+        }
+    }
+
+    public static async Task<SettingsData?> ActualSettingsData(SettingsData settingsData, Logger logger)
+    {
+        if (settingsData == null)
+            return null;
+
+        // check destination
+        if (!Directory.Exists(settingsData.DestinationFolder))
+        {
+            await logger.Error(string.Format("Destination folder \"{0}\" was not found", settingsData.DestinationFolder));
+            //Environment.Exit(1);
+            return null;
+        }
+        await logger.Debug("Destination dirctory was found");
+
+        // get only found sources
+        var actualSources = settingsData.Sources.Where(src => Directory.Exists(src) || File.Exists(src));
+
+        // save found sources 
+        if (actualSources.Any())
+        {
+            // report on not found sources
+            foreach (var src in settingsData.Sources.Except(actualSources))
+                await logger.Warning(string.Format("Source file \"{0}\" was not found", src));
+            settingsData.Sources = actualSources;
+
+            await logger.Debug(string.Format("Approved sources: \n\t{0}", string.Join("\n\t", actualSources)));
+        }
+        else
+        {
+            await logger.Error("Sources was not found");
+            //Environment.Exit(1);
+            return null;
+        }
+
+        // check zip file
+        var zipFullName = Path.Combine(settingsData.DestinationFolder, settingsData.ZipFileName + ".zip");
+
+        await logger.Debug("Zip file full name: " + zipFullName);
+
+        if (File.Exists(zipFullName))
+        {
+            if (YesNoIssue(string.Format("The zip file \"{0}\" is already exists. Do you want rewrite it?", settingsData.ZipFileName)))
+            {
+                File.Delete(zipFullName);
+                await logger.Debug(string.Format("{0} was deleted", zipFullName));
+            }
+            else
+            {
+                var zipFileIndex = Directory.GetFiles(settingsData.DestinationFolder, string.Format("{0}*", settingsData.ZipFileName)).Count();
+                settingsData.ZipFileName += string.Format("({0})", zipFileIndex);
+                await logger.Debug(string.Format("{0} name was approved", settingsData.ZipFileName));
+            }
+        }
+        return settingsData;
+    }
+
+
+    #region Private Methods
+
+    private static async Task CreateDefaultSettingsDataFile(string settingsFileName)
+    {
+        await File.WriteAllTextAsync(settingsFileName, string.Empty);
+        await using var settings = new Settings.Settings
+        (
+            File.Open(settingsFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite)
+        );
+        await settings.WriteSettingsFileAsync(GetDefaultSettingsData());
+    }
+
+    private static SettingsData GetDefaultSettingsData() =>
+        new SettingsData
+        {
+            Sources = new[] { "path/to/source", "path/to/source" },
+            DestinationFolder = "path/to/destination/folder",
+            ZipFileName = "name_of_zip_file",
+            LogOptions = new[] { LogOption.Info, LogOption.Debug, LogOption.Error, LogOption.Warning }
+        };
+
+    private static bool YesNoIssue(string issue)
+    {
+        Console.Write("{0} [y/n]: ", issue);
+        var answer = Console.ReadLine()?.ToLower();
+        return answer?.StartsWith("y") ?? false;
+    }
+
+    #endregion
+}
